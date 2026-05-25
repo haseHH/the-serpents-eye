5a5cd7ccd9d0d2d41517a899ba96e4e9527e8667
haseHH <christian@hase.hamburg>
2026-05-25 22:04:49 +0200

    Link references in the Shrimp-Fried Rice fix

--- kubejs/server_scripts/fix-shrimp-fried-rice.js
+++ kubejs/server_scripts/fix-shrimp-fried-rice.js
@@ -1,4 +1,9 @@
-// The Mantis Shrip is supposed to fry rice, but the unofficial port broke the tag list, so it's frying wheat.
+// The Mantis Shrimp is supposed to fry rice, but the unofficial port broke the tag list, so it's frying wheat.
+
+// original:
+// https://github.com/AlexModGuy/AlexsMobs/blob/1.20/src/main/resources/data/alexsmobs/tags/items/shrimp_rice_fryables.json
+// altered in port for some reason:
+// https://github.com/Raguto/AlexsMobs-1.21.1/blob/master/src/main/resources/data/alexsmobs/tags/item/shrimp_rice_fryables.json
 
 // Add hint on obtainability
 ItemEvents.modifyTooltips(event => {
@@ -9,7 +14,7 @@ ItemEvents.modifyTooltips(event => {
 })
 
 ServerEvents.tags('item', event => {
-  // Remove wheat from the tag (why on earth is it here in the first place?!)
+  // Remove wheat from the tag (why on earth is it here in the first place...)
   event.remove('alexsmobs:shrimp_rice_fryables', 'minecraft:wheat')
   // Restore the original tag mapping, adjusted from the 'forge' to the common 'c' tag
   event.add('alexsmobs:shrimp_rice_fryables', '#c:crops/rice')
